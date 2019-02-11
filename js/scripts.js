185edc9fb9d2ef940e0c1d50c0d5c41cfa381b21
Aya Abu <someone990@yahoo.com>
2019-02-10 23:51:21 -0800

    add the 3 buttons with some changes of colors of messages attached to handlers

---
+++ js/scripts.js
@@ -0,0 +1,35 @@
+$("document").ready(function(){
+  $("button#hello").click(function(){
+    $("ul").prepend("<li>Hello!</li>");
+    $("ul#webpage").children("li").first().click(function() {
+      $(this).css("background-color","green");
+    });
+  });
+
+  $("button#Good-bye").click(function(){
+    $("ul").prepend("<li>Good Bye there!</li>");
+    $("ul#user").children().first().css("background-color","yellow");
+    $("ul#webpage").children("li").first().click(function() {
+      $(this).css("background-color","green");
+    });
+  });
+
+  $("button#Stop").click(function(){
+    $("ul").prepend("<li>Stop it!</li>");
+    $("ul#webpage").children("li").first().click(function() {
+      $(this).css("background-color","green");
+    });
+  });
+
+  // $("ul#user").children("li").first().click(function(){
+  //   $("li").click(function(){
+  //     alert("Hi");
+  //   });
+  //  $("ul#user").children().css("background-color","green");
+//  });
+
+//  $("ul#webpage").children("li").click(function() {
+//    $(this).css("background-color","green");
+//  });
+
+});
